1b1c03cd4dd1991988f7e898c893262f738af393
Carlos Diaz <bycdiaz@icloud.com>
2023-09-23 15:40:23 -0700

    Sets app child.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import './App.css'
+import Header from './components/Header'
 
-function App() {
+function App(props: {
+  child?: JSX.Element
+}) {
 
   return (
     <div>
-      wow
+      <Header pageTitle='Pokemon Team Builder' />
+      <h2>
+        {content()}
+      </h2>
     </div>
   )
+
+  function content() {
+    if (props.child) {
+      return props.child;
+    }
+
+    return 'Welcome to the Pokemon Team Builder. Wow!';
+  }
 }
 
 export default App
