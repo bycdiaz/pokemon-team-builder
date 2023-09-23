e706282fcebb1b15cb16e177d6670532d55a4f12
Carlos Diaz <bycdiaz@icloud.com>
2023-09-23 15:36:33 -0700

    Adds initial routing.

---
+++ src/Router.tsx
@@ -0,0 +1,60 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import ContentContainer from "./components/ContentContainer";
+import { featured, search, team } from "./data/types";
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      // errorElement: <ErrorPage />,
+    },
+    {
+      path: `/${search}`,
+      element: (
+        <App
+          child={
+            <ContentContainer
+              sectionName={search}
+            />
+          }
+        />
+      )
+      ,
+      // errorElement: <ErrorPage />,
+    },
+    {
+      path: `/${featured}`,
+      element: (
+        <App
+          child={
+            <ContentContainer
+              sectionName={featured}
+            />
+          }
+        />
+      )
+      ,
+      // errorElement: <ErrorPage />,
+    },
+    {
+      path: `/${team}`,
+      element: (
+        <App
+          child={
+            <ContentContainer
+              sectionName={team}
+            />
+          }
+        />
+      )
+      ,
+      // errorElement: <ErrorPage />,
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
