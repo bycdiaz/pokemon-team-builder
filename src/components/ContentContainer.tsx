22b810f36695acc5dbade3d7037408a2094894d5
Carlos Diaz <bycdiaz@icloud.com>
2023-09-24 15:26:11 -0700

    Adjusts content container.

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -1,11 +1,18 @@
 import { NavigationOption, userReadableNavigationOptions } from "../data/types";
+import Featured from "./Featured";
 
 function ContentContainer(props: { sectionName: NavigationOption }) {
-  return (
-    <div>
-      {userReadableNavigationOptions[props.sectionName]}
-    </div>
-  )
+  const section = props.sectionName;
+
+  if (section === 'featured') {
+    return <Featured />;
+  } else {
+    return (
+      <div>
+        {userReadableNavigationOptions[props.sectionName]}
+      </div>
+    )
+  }
 }
 
 export default ContentContainer;
