b17cc7411054f681025fee35cac575a4e7f3e7fb
Carlos Diaz <cdiaz@amplify.com>
2023-09-30 16:04:28 -0700

    Shows total cost in cart.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,20 +1,26 @@
 import { useContext } from "react";
 import { ItemCartContext } from "../Context";
 import ItemCard from "./ItemCard";
+import { totalCost } from "../helpers";
 
 function Cart() {
   const { itemsCart } = useContext(ItemCartContext);
-  console.log(itemsCart);
-
   return (
-    <div className="items-in-cart">
-      {
-        Object.values(itemsCart).map(item => (
-          <ItemCard item={item.item} />
-        ))
-      }
+    <div className="cart-container">
+      <div className="cart-total-cost">
+        {`Total Cost: ${totalCost(itemsCart)}`}
+      </div>
+      <div className="items-in-cart">
+        {
+          Object.values(itemsCart).map(item => (
+            <ItemCard item={item.item} />
+          ))
+        }
+      </div>
     </div>
   );
+
+
 }
 
 export default Cart;
