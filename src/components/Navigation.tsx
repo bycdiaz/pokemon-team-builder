dd811dd0598b226d7b532e73940728eed8953105
Carlos Diaz <cdiaz@amplify.com>
2023-09-30 15:19:19 -0700

    Adds cart count.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,22 +1,32 @@
 import { Link } from "react-router-dom";
 import { NavigationOption, userReadableNavigationOptions } from "../data/types";
+import { useContext } from "react";
+import { ItemCartContext } from "../Context";
 
 function Navigation(props: {
   navOptions: readonly NavigationOption[]
 }) {
+  const { itemsCart } = useContext(ItemCartContext);
 
   return (
     <div>
       {props.navOptions.map(option => {
         return (
-          <Link
-            to={setLink(option)}
-            key={option}
-          >
-            <div>
-              {userReadableNavigationOptions[option]}
+          <div className="navigation-link">
+            <Link
+              to={setLink(option)}
+              key={option}
+            >
+              <div>
+                {userReadableNavigationOptions[option]}
+              </div>
+            </Link>
+            <div
+              className="cart-count"
+            >
+              {option === 'cart' && Object.keys(itemsCart).length}
             </div>
-          </Link>
+          </div>
         )
       })}
     </div>
