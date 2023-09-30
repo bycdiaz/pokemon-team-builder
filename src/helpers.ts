b72c96a2873e6c515bed2da96adaeb228d7c73c8
Carlos Diaz <cdiaz@amplify.com>
2023-09-30 13:26:30 -0700

    Adds helper.

---
+++ src/helpers.ts
@@ -0,0 +1,13 @@
+export function randomNumberGenerator() {
+  const randomNumbers: number[] = [];
+
+  while (randomNumbers.length < 10) {
+    const randomNumber = Math.floor(Math.random() * 2110) + 1;
+
+    if (!randomNumbers.includes(randomNumber)) {
+      randomNumbers.push(randomNumber);
+    }
+  }
+
+  return randomNumbers;
+}
